9ce3fcd29f71d3c1aadc5533592ce714027a181f
chenfunnel <15339233158@189.cn>
2017-05-02 18:15:05 +0800

    实现了计划存入的代码

--- shopsys/apps/plan/views.py
+++ shopsys/apps/plan/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.shortcuts import render,get_object_or_404
-from shopsys.apps.plan.forms import  PlanForm,PlaneForm,TrainForm,HotelForm,ShemeForm
+from shopsys.apps.plan.forms import  PlanForm,PlaneForm,TrainForm,HotelForm
 from django.http import HttpRequest,HttpResponse,HttpResponseRedirect
-from shopsys.apps.plan.models import Plan,Scheme
+from shopsys.apps.plan.models import Plan
 
 # Create your views here.
 def plan(request):
     Method = request.method
+
     if Method == 'POST':
         #如果有post提交的动作，就将post中的数据赋值给uf，供该函数使用
+
         uf = PlanForm(request.POST)
+        print(uf)
         if uf.is_valid():
-            name=uf.cleaned_data['fromcity']+uf.cleaned_data['toctiy']+uf.cleaned_data['start_at']
+            print ('11')
+            name=uf.cleaned_data['fromcity']+uf.cleaned_data['tocity']+uf.cleaned_data['start_at'].strftime("%Y%m%d")
             fromcity = uf.cleaned_data['fromcity']
-            tocity = uf.cleaned_data['toctiy']
+            tocity = uf.cleaned_data['tocity']
             start_at=uf.cleaned_data['start_at']
             back_at = uf.cleaned_data['back_at']
             traffictype = uf.cleaned_data['traffictype']
@@ -23,9 +27,12 @@ def plan(request):
             ifback = uf.cleaned_data['ifback']
             description = uf.cleaned_data['description']
             is_active=1
+
+            print (name)
+
             planAdd = Plan.objects.create(name=name,fromcity=fromcity,tocity=tocity,start_at=start_at,back_at=back_at,traffictype=traffictype,hotletype=hotletype,target=target,targetcoordinate=targetcoordinate,ifback=ifback,description=description,is_active=is_active)
             print(planAdd)
-            return render (request,'plan/plan_detail_1.html',locals())
+            return render (request,'plan/plan_detail.html',locals())
 
     else:
         uf = PlanForm()
